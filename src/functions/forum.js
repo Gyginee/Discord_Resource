67528d214eb7a443de960fe1da5d30b6bb7e2d27
Gyginee <hungnguyenminh66@gmail.com>
2023-10-31 04:34:52 +0700

    delay add

--- src/functions/forum.js
+++ src/functions/forum.js
@@ -1,6 +1,9 @@
 
 const { fetchData, updateForumId } = require('./getsheets')
 
+async function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 async function updateForum(forumId) {
     /**
@@ -12,30 +15,30 @@ async function updateForum(forumId) {
     if (data) {
         let i = 2;
         let firstItemSkipped = false; // Tạo một biến để xác định xem đã bỏ qua phần tử đầu tiên hay chưa
-         for (const item of data) {
+        for (const item of data) {
             if (!firstItemSkipped) {
                 firstItemSkipped = true;
                 continue; // Bỏ qua việc xử lý cho phần tử đầu tiên
             }
 
-             const discordId = item[0];
-             const name = item[1];
-             const version = item[2];
-             const os = item[3];
-             const software = item[4];
-             const size = item[5];
-             const download = item[6];
-             const en = item[7];
-             const vn = item[8];
-             const author = item[9];
-             const imageUrl = item[10]; // Replace with the specific image URL
+            const discordId = item[0];
+            const name = item[1];
+            const version = item[2];
+            const os = item[3];
+            const software = item[4];
+            const size = item[5];
+            const download = item[6];
+            const en = item[7];
+            const vn = item[8];
+            const author = item[9];
+            const imageUrl = item[10]; // Replace with the specific image URL
 
-             const threadName = name;
-             console.log(imageUrl);
-             if (Array.isArray(item)) {
-                 const contentArr = `${vn}\nINFO\n•  Size: ${size}\n• ${software}\n• ${os}\n--------------------------------------------------------------------------------------\n🔥 [GET HERE](${download})`;
-                 const embeds = [];
-                 if (imageUrl && imageUrl !== '' && imageUrl !== 'Loading...' && imageUrl !== 'Đang tải...' && imageUrl !== '#N/A' && imageUrl !== '#VALUE!') {
+            const threadName = name;
+            //console.log(imageUrl);
+            if (Array.isArray(item)) {
+                const contentArr = `${vn}\nINFO\n•  Size: ${size}\n• ${software}\n• ${os}\n--------------------------------------------------------------------------------------\n🔥 [GET HERE](${download})`;
+                const embeds = [];
+                if (imageUrl && imageUrl !== '' && imageUrl !== 'Loading...' && imageUrl !== 'Đang tải...' && imageUrl !== '#N/A' && imageUrl !== '#VALUE!') {
                     embeds.push({
                         image: {
                             url: imageUrl,
@@ -47,8 +50,8 @@ async function updateForum(forumId) {
                             url: 'https://cdn.discordapp.com/attachments/1154392823966208111/1165349160027967628/standard_2.gif?ex=654fc17a&is=653d4c7a&hm=edc33672e21539e17721b1a596700d72472c87c6b1f5d1c7da91dc1fb905b3eb&',
                         },
                     });
-                }                
-                 thread = await forumId.threads.create({
+                }
+                thread = await forumId.threads.create({
                     name: threadName,
                     message: {
                         content: contentArr,
@@ -58,16 +61,22 @@ async function updateForum(forumId) {
                 },);
 
                 thread.send(`• ${version}`).catch(console.error);
-                console.log("done up post");
-               
+
+
                 const threadId = thread.id;
                 const cell = `A${i}`
                 updateForumId(cell, threadId)
                 i++;
-             } else {
-                 console.error('Invalid item:', item);
-             }
-         }
+
+                // Nếu index chia hết cho 15 và không phải là phần tử cuối cùng, thì tạo một độ trễ
+                if ((index + 1) % 15 === 0 && index !== data.length - 1) {
+                    await delay(2000); // Đợi 2 giây
+                }
+            } else {
+                console.error('Invalid item:', item);
+            }
+        }
+        console.log("done up post");
 
         /* --devtest */
 
